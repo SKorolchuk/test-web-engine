8288e4d78122d0173fe1a5375ac9aa6fb708ea53
Serge Korolchuk <sergey.korolchuk.dev@gmail.com>
2014-04-17 12:52:34 +0300

    Issue #5: Added basic realization of statistics service

---
+++ TestWebEngine/TestWebEngine.Service.Statistics/IStatisticsService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace TestWebEngine.Service.Statistics
+{
+    [ServiceContract]
+    public interface IStatisticsService
+    {
+        [OperationContract]
+        bool UpdateStatistics(DateTime? date, double? score);
+    }
+
+
+    [DataContract]
+    public class StatisticsType
+    {
+        [DataMember]
+        public DateTime? LastTestCompleted { get; set; }
+
+        [DataMember]
+        public double AverageTestScore { get; set; }
+
+        [DataMember]
+        public long? CompletedTestsCount { get; set; }
+    }
+}
---
+++ TestWebEngine/TestWebEngine.Service.Statistics/StatisticsService.svc.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace TestWebEngine.Service.Statistics
+{
+    public class StatisticsService : IStatisticsService
+    {
+        private readonly StatisticsType _statistics;
+
+        public StatisticsService()
+        {
+            _statistics = new StatisticsType();
+        }
+
+        public bool UpdateStatistics(DateTime? date, double? score)
+        {
+            if (date == null)
+            {
+                throw new ArgumentNullException("date");
+            }
+            else if (score == null)
+            {
+                throw new ArgumentNullException("score");
+            }
+            _statistics.CompletedTestsCount++;
+            _statistics.LastTestCompleted = date;
+            if (_statistics.CompletedTestsCount != null)
+                _statistics.AverageTestScore += score.Value/_statistics.CompletedTestsCount.Value;
+            else
+            {
+                _statistics.AverageTestScore = score.Value;
+            }
+            return true;
+        }
+    }
+}
